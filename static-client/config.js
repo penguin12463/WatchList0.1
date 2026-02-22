fc5fa5cec222bf81f21385be09d8106700024c83
Adam Barry <adambarry@Adams-MacBook-Air.local>
2026-02-22 17:18:59 +0000

    Configure Supabase anon key for static client

--- static-client/config.js
+++ static-client/config.js
@@ -1,3 +1,3 @@
 // Fill these values from Supabase project settings.
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://itozjiieewburafwwqhm.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0b3pqaWllZXdidXJhZnd3cWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NzU4MDAsImV4cCI6MjA4NzM1MTgwMH0.NlJ3dUfLgTVmOM3yTetEwZ1ms-e41eRCt2vkBUj-7Uk";
